814f5141e22e2edc6814680c410fb581fbc55a37
Martin Omwenga <omwenga.mrtn@gmail.com>
2024-06-25 12:22:17 +0000

    Adjusted tests and added oauth

--- genesis-vocali/modules/auth/register-service.ts
+++ genesis-vocali/modules/auth/register-service.ts
@@ -2,12 +2,13 @@ import supabase from '@/services/auth/auth-supabase.service';
 
 export async function registerUser(email: string, password: string) {
   try {
-    const { user, error } = await supabase.auth.signUp({
+    const { data, error } = await supabase.auth.signUp({
       email,
       password,
     });
     if (error) throw error;
-    return user;
+
+    return {user: data.user, token: data.session?.access_token };
   } catch (error) {
     console.error('Registration error:', error);
     throw error;
--- genesis-vocali/services/auth/auth-supabase.service.ts
+++ genesis-vocali/services/auth/auth-supabase.service.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 import config from "@/resources/utils/config";
 
-const supabaseUrl = config.SUPABASE_URL;
-const supabaseAnonKey = config.SUPABASE_API_KEY;
+const supabaseUrl = (config.SUPABASE_URL) as string;
+const supabaseAnonKey = (config.SUPABASE_API_KEY) as string;
 
 export default createClient(supabaseUrl, supabaseAnonKey);
