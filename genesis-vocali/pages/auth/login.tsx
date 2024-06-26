2b752756df721dd0244ede311580f72ec9e521c6
Martin Omwenga <omwenga.mrtn@gmail.com>
2024-06-26 15:38:38 +0000

    Added i18n

--- genesis-vocali/pages/auth/login.tsx
+++ genesis-vocali/pages/auth/login.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { useTranslations } from 'next-intl';
+import { getStaticPropsWithTranslations } from '@/modules/lang/props';
+
+export const getStaticProps = getStaticPropsWithTranslations;
 
 const Login: React.FC = () => {
-  return <h1>Login Page</h1>;
+  const t = useTranslations('common');
+
+  return <h1>{t('login')}</h1>;
 };
 
 export default Login;
