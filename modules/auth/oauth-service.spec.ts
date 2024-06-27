73a9076ed666f0259b8969992721d8abeb645efe
Martin Omwenga <omwenga.mrtn@gmail.com>
2024-06-27 17:05:39 +0000

    Register and login page cosmetics done

--- modules/auth/oauth-service.spec.ts
+++ modules/auth/oauth-service.spec.ts
@@ -2,12 +2,12 @@
  * @jest-environment node
  */
 
-import { oauthLogin } from "./oauth-service";
+import { oauth } from "./oauth-service";
 
 describe("oauthLogin (Integration Test)", () => {
   it("successfully logs in with a valid provider", async () => {
     const provider = 'google';
-    const result = await oauthLogin(provider);
+    const result = await oauth(provider);
     console.log(result);
 
     expect(result).toHaveProperty('provider');
@@ -16,6 +16,6 @@ describe("oauthLogin (Integration Test)", () => {
 
   it("throws an error with an invalid provider", async () => {
     const invalidProvider = 'invalid-provider';
-    await expect(oauthLogin(invalidProvider)).rejects.toThrow('Invalid provider: invalid-provider');
+    await expect(oauth(invalidProvider)).rejects.toThrow('Invalid provider: invalid-provider');
   });
 });
