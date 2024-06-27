dbd98d0bfad545964d0d2790caae1110e8c952ae
Martin Omwenga <omwenga.mrtn@gmail.com>
2024-06-27 19:37:30 +0000

    Updated login page

---
+++ pages/app/dashboard.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { useTranslations } from 'next-intl';
+import { getStaticPropsWithTranslations } from '@/modules/lang/props';
+
+export const getStaticProps = getStaticPropsWithTranslations;
+
+const Dashboard: React.FC = () => {
+  const t = useTranslations('common');
+
+  return <h1>{t('Dashboard')}</h1>;
+};
+
+export default Dashboard;
--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,16 +1,21 @@
-import React from "react";
-import Image from "next/image";
-import { useTranslations } from "next-intl";
-import RegisterForm from "@/resources/inputs/register-form";
-import register_page_style from "@/styles/pages/register.module.css";
-import { getStaticPropsWithTranslations } from "@/modules/lang/props";
+import React from 'react';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+import { useTranslations } from 'next-intl';
+import LoginForm from '@/resources/inputs/login-form';
+import register_page_style from '@/styles/pages/register.module.css';
+import { getStaticPropsWithTranslations } from '@/modules/lang/props';
 
 export const getStaticProps = getStaticPropsWithTranslations;
 
 const LoginPage: React.FC = () => {
-  const t = useTranslations("common");
-  const handleRegisterSuccess = (user: any, token: string) => {
-    console.log("Login successful:", user, token);
+  const router = useRouter();
+  const t = useTranslations('common');
+  const handleLoginSuccess = (user: any, token: string) => {
+    console.log('Login successful:', user, token);
+    localStorage.setItem('token', token);
+    localStorage.setItem('user', JSON.stringify(user));
+    router.push('/app/dashboard');
   };
 
   return (
@@ -27,17 +32,10 @@ const LoginPage: React.FC = () => {
       </div>
       <div className={register_page_style.rightColumn}>
         <div className={register_page_style.rightColumnContent}>
-          <Image
-            src="/user.svg"
-            alt="Login and Register"
-            width={45}
-            height={45}
-          />
-          <h1 className={register_page_style.title}>{t("Login")}</h1>
-          <RegisterForm onRegisterSuccess={handleRegisterSuccess} />
-          <p className={register_page_style.login_text}>
-            {t("Dont have an account?")} <strong> {t("Register")} </strong>
-          </p>
+          <Image src="/user.svg" alt="Login and Register" width={45} height={45} />
+          <h1 className={register_page_style.title}>{t('Login')}</h1>
+          <LoginForm onLoginSuccess={handleLoginSuccess} />
+          <p className={register_page_style.login_text}>{t('Dont have an account?')} <strong> {t('Register')} </strong></p>
         </div>
       </div>
     </div>
