3f27207d5f052c353f71c16477f27684c7e5d4f4
Martin Omwenga <omwenga.mrtn@gmail.com>
2024-06-27 10:30:46 +0000

    Updated folder structure

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  i18n: {
+    locales: ['en', 'es'],
+    defaultLocale: 'en'
+  }
 };
 
 export default nextConfig;
