95f408d109136c5c45b3345d1053ab0bacb80ec6
Martin Omwenga <omwenga.mrtn@gmail.com>
2024-06-28 19:15:36 +0000

    Refactored oauth

--- modules/auth/oauth-service.ts
+++ modules/auth/oauth-service.ts
@@ -1,17 +1,10 @@
 import supabase from '@/services/auth/auth-supabase.service';
 import { Provider } from '@supabase/supabase-js';
 
-// Define the allowed provider names
-const validProviders: Provider[] = ['github', 'gitlab', 'google'];
-
-export async function oauth(provider: string) {
-  if (!validProviders.includes(provider as Provider)) {
-    throw new Error(`Invalid provider: ${provider}`);
-  }
-
+export async function oauth(provider: Provider) {
   try {
     const { data, error } = await supabase.auth.signInWithOAuth({
-      provider: provider as Provider,
+      provider: provider,
     });
     if (error) throw error;
 
