748e9aa3598af1b22b37f6759793ea39dcbdd5b4
Martin Omwenga <omwenga.mrtn@gmail.com>
2024-06-29 08:56:46 +0000

    Updated clear

--- services/auth/auth-supabase.service.ts
+++ services/auth/auth-supabase.service.ts
@@ -1,7 +1,38 @@
-import { createClient } from '@supabase/js';
-import config from '@/resources/config';
+import { createClient, SupabaseClient, User } from '@supabase/supabase-js';
+import config from '@/resources/utils/config';
 
-const supabaseUrl = config.SUPABASE_URL;
-const supabaseAnonKey = config.SUPABASE_API_KEY;
+class AuthService {
+  private supabase: SupabaseClient;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+  constructor() {
+    const supabaseUrl = config.SUPABASE_URL as string;
+    const supabaseAnonKey = config.SUPABASE_API_KEY as string;
+    this.supabase = createClient(supabaseUrl, supabaseAnonKey);
+  }
+
+  async registerUser(email: string, password: string): Promise<{ user: User | null; token: string | undefined }> {
+    const { data, error } = await this.supabase.auth.signUp({
+      email,
+      password,
+    });
+    if (error) throw error;
+    
+    return { user: data.user, token: data.session?.access_token };
+  }
+
+  async loginUser(email: string, password: string): Promise<{ user: User | null; token: string | undefined }> {
+    const { data, error } = await this.supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+    if (error) throw error;
+  
+    return { user: data.user, token: data.session?.access_token };
+  }
+
+  getSupabaseClient(): SupabaseClient {
+    return this.supabase;
+  }
+}
+
+export default new AuthService();
