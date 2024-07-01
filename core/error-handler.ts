7f8d7b2f1174df2a8d4b45d0a6e83eaf5d624bd7
Martin Omwenga <omwenga.mrtn@gmail.com>
2024-07-01 18:46:53 +0000

    Updated auth service

---
+++ core/error-handler.ts
@@ -0,0 +1,6 @@
+// import { toast } from 'react-toastify';
+
+export default (error: string) => {
+  console.error("Error occurred:", error);
+  // toast.error(error.message || "An unexpected error occurred");
+};
