1a3c94305f6f4d9020ed563cf547619a4b98ce9c
Martin Omwenga <omwenga.mrtn@gmail.com>
2024-07-02 06:21:12 +0000

    Updated AuthService

--- services/auth/auth-supabase.service.ts
+++ services/auth/auth-supabase.service.ts
@@ -1,7 +1,58 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient, SupabaseClient, User } from "@supabase/supabase-js";
+import { Provider } from '@supabase/supabase-js';
 import config from "@/resources/utils/config";
+import errorHandler from "@/core/error-handler";
 
-const supabaseUrl = (config.SUPABASE_URL) as string;
-const supabaseAnonKey = (config.SUPABASE_API_KEY) as string;
+class AuthService {
+  private supabase: SupabaseClient;
 
-export default createClient(supabaseUrl, supabaseAnonKey);
+  constructor() {
+    const supabaseUrl = config.SUPABASE_URL as string;
+    const supabaseAnonKey = config.SUPABASE_API_KEY as string;
+    this.supabase = createClient(supabaseUrl, supabaseAnonKey);
+  }
+
+  async registerUser(
+    email: string,
+    password: string,
+  ): Promise<{ user: User | null; token: string | undefined } | null> {
+    const { data, error } = await this.supabase.auth.signUp({
+      email,
+      password,
+    });
+    if (error) return errorHandler(error.message);
+
+    return { user: data.user, token: data.session?.access_token };
+  }
+
+  async loginUser(
+    email: string,
+    password: string,
+  ): Promise<{ user: User | null; token: string | undefined } | null> {
+    const { data, error } = await this.supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+    if (error) return errorHandler(error.message);
+
+    return { user: data.user, token: data.session?.access_token };
+  }
+
+  async oauth(provider: Provider): Promise<{provider: Provider; url: string;} | null> {
+    const { data, error } = await this.supabase.auth.signInWithOAuth({
+      provider: provider,
+      options: {
+        redirectTo: `${window.location.origin}/app/dashboard`
+      }
+    });
+    if (error) return errorHandler(error.message);
+
+    return data;
+  }
+
+  getSupabaseClient(): SupabaseClient {
+    return this.supabase;
+  }
+}
+
+export default new AuthService();
