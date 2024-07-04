67de5e98d27c152e7b32bf281ba4691db83bf9c2
Martin Omwenga <omwenga.mrtn@gmail.com>
2024-07-04 08:25:13 +0000

    Returned to using default Toaster

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,8 @@ import "@/styles/globals.css";
 import { AppProps } from "next/app";
 import { NextIntlClientProvider } from "next-intl";
 import { useRouter } from "next/router";
-import { Toaster } from "@/resources/containers/translated-toaster";
+import { Toaster } from "react-hot-toast";
+// import { Toaster } from "@/resources/containers/translated-toaster";
 import { getStaticPropsWithTranslations } from "@/modules/lang/props";
 
 function App({ Component, pageProps }: AppProps) {
