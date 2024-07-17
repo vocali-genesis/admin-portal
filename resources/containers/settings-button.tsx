cdf9631ffcc05253b2dbeb5dcc03a8e68bd5c59b
Martin Omwenga <omwenga.mrtn@gmail.com>
2024-07-17 19:00:15 +0000

    Updated settings functionality

--- resources/containers/settings-button.tsx
+++ resources/containers/settings-button.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
+import { useRouter } from 'next/router';
 import { useTranslations } from "next-intl";
 import settings_btn_styles from "./styles/settings-button.module.css";
 
 const SettingsButton = () => {
   const t = useTranslations();
+  const router = useRouter();
 
   return (
-    <button className={settings_btn_styles.settingsButton}>
+    <button className={settings_btn_styles.settingsButton} onClick={() => router.push('/settings/settings')}>
       <Image
         className={"pt-1"}
         src="/settings.svg"
