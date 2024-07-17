8ad63566a5d6826a57a6ace76023a2b5c286327f
Martin Omwenga <omwenga.mrtn@gmail.com>
2024-07-17 19:00:15 +0000

    Updated settings functionality

--- core/forms/schemas/auth-schema.ts
+++ core/forms/schemas/auth-schema.ts
@@ -43,3 +43,18 @@ export const register_schema = yup.object().shape({
     .oneOf([yup.ref("password"), undefined], "Passwords must match")
     .required("Confirm Password is required"),
 });
+
+export const settings_schema = yup.object().shape({
+  email: yup
+    .string()
+    .email("Invalid email format")
+    .required("Email is required"),
+  password: yup
+    .string()
+    .min(8, "Password must be at least 8 characters")
+    .required("Password is required"),
+  confirm_password: yup
+    .string()
+    .oneOf([yup.ref("password"), undefined], "Passwords must match")
+    .required("Confirm Password is required"),
+});
--- modules/settings/settings.tsx
+++ modules/settings/settings.tsx
@@ -1,23 +1,36 @@
 import React, { useState } from "react";
 import { useTranslations } from "next-intl";
-import SideBar from "@/core/components/sidebar";
+import { useForm } from "react-hook-form";
 import { getStaticPropsWithTranslations } from "@/modules/lang/props";
 import { GlobalCore } from "@/core/module/module.types";
 import settings_styles from "./styles/settings.module.css";
+import messageHandler from "@/core/message-handler";
+import AuthService from "@/services/auth/auth-supabase.service";
+import { settings_schema } from "@/core/forms/schemas/auth-schema";
+import { yupResolver } from "@hookform/resolvers/yup";
 import { FaTrash } from "react-icons/fa6";
+import { GetStaticProps } from "next";
 
-export const getStaticProps = getStaticPropsWithTranslations;
+export const getStaticProps: GetStaticProps = getStaticPropsWithTranslations;
 
 const Settings = () => {
   const t = useTranslations("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
   const [language, setLanguage] = useState("en");
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("Form submitted", { email, password, language });
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(settings_schema),
+  });
+
+  const onSubmit = async (data: any) => {
+    const updatedUser = await AuthService.updateUser(data.email, data.password);
+
+    if (updatedUser)
+      messageHandler.handleSuccess("Profile updated successfully");
   };
 
   const handleDeleteAccount = () => {
@@ -28,39 +41,53 @@ const Settings = () => {
     <div className={settings_styles.container}>
       <div className={settings_styles.contentWrapper}>
         <main className={settings_styles.mainContent}>
-          <form onSubmit={handleSubmit} className={settings_styles.form}>
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className={settings_styles.form}
+          >
             <div className={settings_styles.mainFormGroup}>
               <div className={`${settings_styles.formGroup}`}>
-                <label htmlFor="email">{t("Email")}:</label>
-                <input
-                  type="email"
-                  id="email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  required
-                />
+                <div className={settings_styles.inputContainer}>
+                  <label htmlFor="email">{t("Email")}:</label>
+                  <input type="email" id="email" {...register("email")} />
+                </div>
+                {errors.email && (
+                  <span className={settings_styles.errorMessage}>
+                    {errors.email.message}
+                  </span>
+                )}
               </div>
               <div className={`${settings_styles.formGroup}`}>
-                <label htmlFor="password">{t("Password")}:</label>
-                <input
-                  type="password"
-                  id="password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  required
-                />
+                <div className={settings_styles.inputContainer}>
+                  <label htmlFor="password">{t("New Password")}:</label>
+                  <input
+                    type="password"
+                    id="password"
+                    {...register("password")}
+                  />
+                </div>
+                {errors.password && (
+                  <span className={settings_styles.errorMessage}>
+                    {errors.password.message}
+                  </span>
+                )}
               </div>
               <div className={`${settings_styles.formGroup}`}>
-                <label htmlFor="confirm-password">
-                  {t("Confirm Password")}:
-                </label>
-                <input
-                  type="password"
-                  id="confirm-password"
-                  value={confirmPassword}
-                  onChange={(e) => setConfirmPassword(e.target.value)}
-                  required
-                />
+                <div className={settings_styles.inputContainer}>
+                  <label htmlFor="confirm-password">
+                    {t("Confirm Password")}:
+                  </label>
+                  <input
+                    type="password"
+                    id="confirm-password"
+                    {...register("confirm_password")}
+                  />
+                </div>
+                {errors.confirm_password && (
+                  <span className={settings_styles.errorMessage}>
+                    {errors.confirm_password.message}
+                  </span>
+                )}
               </div>
             </div>
 
@@ -69,7 +96,6 @@ const Settings = () => {
             <div className={settings_styles.socialLogins}>
               <button
                 type="button"
-                onClick=""
                 className={`${settings_styles.oauthButton} ${settings_styles.googleOAuthButton}`}
               >
                 <p>
@@ -84,14 +110,13 @@ const Settings = () => {
 
             <div className={settings_styles.divider} />
 
-            <div className={settings_styles.formGroup}>
-              <label style={{ paddingTop: "5vh" }} htmlFor="language">
-                {t("Language")}:
-              </label>
+            <div className={settings_styles.languageGroup}>
+              <label htmlFor="language">{t("Language")}:</label>
               <select
                 id="language"
                 value={language}
                 onChange={(e) => setLanguage(e.target.value)}
+                className={settings_styles.languageSelect}
               >
                 <option value="en">EN</option>
                 <option value="es">ES</option>
