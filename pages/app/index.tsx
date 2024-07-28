24522c46d62a90df8a79ae4885f3db4c2cb7a80c
Muhammad Naveed <deevan.naveed@gmail.com>
2024-07-29 04:13:50 +0700

    settings subscription component added and rout to subscriptio

--- pages/app/index.tsx
+++ pages/app/index.tsx
@@ -2,15 +2,15 @@ import { ModuleManager } from "@/core/module/module.manager";
 import Spinner from "@/resources/containers/spinner";
 
 export async function getServerSideProps() {
-    return {
-        redirect: {
-            destination: '/app/' + ModuleManager.get().components.defaultApp(),
-            permanent: false,
-        },
-    };
+  return {
+    redirect: {
+      destination: "/app/" + ModuleManager.get().components.defaultApp(),
+      permanent: false,
+    },
+  };
 }
 
 function DefaultApp() {
-    return <Spinner />
+  return <Spinner />;
 }
 export default DefaultApp;
