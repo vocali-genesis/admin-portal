b6718d863e12243c6984d032476fdd50d19f6bfa
Martin Omwenga <omwenga.mrtn@gmail.com>
2024-08-06 16:14:29 +0000

    Netlify build fix

--- core/module/service.factory.ts
+++ core/module/service.factory.ts
@@ -1,19 +1,21 @@
 import { useState } from "react";
 import { ModuleManager } from "./module.manager";
 import { ServiceInterface, ServiceName } from "./services.types";
-
+import MessageHandler from "@/core/message-handler";
 /**
  * Service factory uses Module Manager under the hook
  */
 
+const messageHandler = MessageHandler.get();
+
 export default class Service {
   public static get<S extends ServiceName>(
-    name: S
+    name: S,
   ): ServiceInterface<S> | undefined {
     const service = ModuleManager.get().components.services(name);
     if (!service) {
       throw new Error(
-        `Service ${name} not init, have you imported in the config.json? Have you 'pnpm load' the config.json?`
+        `Service ${name} not init, have you imported in the config.json? Have you 'pnpm load' the config.json?`,
       );
     }
     return service as ServiceInterface<S>;
@@ -24,8 +26,14 @@ export default class Service {
  * Hook
  */
 export const useService = <S extends ServiceName>(
-  serviceName: S
-): ServiceInterface<S> => {
+  serviceName: S,
+): ServiceInterface<S> | null => {
   const [service] = useState(Service.get(serviceName));
-  return service;
+  if (service) {
+    return service;
+  } else {
+    return messageHandler.handleError(
+      `Service ${serviceName} not init, have you imported in the config.json? Have you 'pnpm load' the config.json?`,
+    );
+  }
 };
