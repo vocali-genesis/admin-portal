851f6afb7897b5f40008ae94d800e73e31c8d42e
Abel Bordonado <abelbordonado@gmail.com>
2024-08-11 21:26:16 +0800

    Fixed build issues and added a Seed Class

--- resources/tests/test.utils.ts
+++ resources/tests/test.utils.ts
@@ -1,15 +1,19 @@
 import { AuthService } from "@/core/module/services.types";
 import { faker } from "@faker-js/faker";
+import { Seed } from "./seed";
+import { GenesisUser } from "@/core/module/core.types";
 
 /**
  * Some useful test utils to make the testing view simple
  */
-export const login = (
+export const login = async (
   authService: AuthService,
-  { email }: { email?: string } = {}
+  props: Parameters<Seed["user"]>[0] = {}
 ) => {
-  authService.loginUser(
-    email || faker.internet.email(),
+  const { user, token } = (await authService.loginUser(
+    props.email || faker.internet.email(),
     faker.internet.password()
-  );
+  )) as { user: GenesisUser; token: string };
+
+  return { user: { ...user, ...Seed.new().user(props) }, token };
 };
