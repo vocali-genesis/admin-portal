b67be89776c5cc86c48399047845e12ba6a4360a
Abel Bordonado <abelbordonado@gmail.com>
2024-08-11 20:42:14 +0800

    Fixed eslint

--- pages/app/[slug].tsx
+++ pages/app/[slug].tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useRouter } from "next/router";
 import { ModuleManager } from "@/core/module/module.manager";
 import Navbar from "@/core/components/nav";
