bfe866d58e713f13befc6822fc11ac650db1f5f8
Abel Bordonado <abelbordonado@gmail.com>
2024-09-01 14:28:01 +0800

    removed double :

--- modules/auth/saas/login.tsx
+++ modules/auth/saas/login.tsx
@@ -23,7 +23,7 @@ const Login = () => {
       <OauthButtons
         label={
           <>
-            <strong>{t("auth.login")}</strong> {t("auth.with-others")}:
+            <strong>{t("auth.login")}</strong> {t("auth.with-others")}
           </>
         }
       />
