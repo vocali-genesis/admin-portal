ae918b57ba7acb2f3d4bcc4f4b896b9489d348dd
omwenga.mrtn <omwenga.mrtn@gmail.com>
2024-09-09 19:56:27 +0000

    Updated select component

--- resources/inputs/basic-select.input.tsx
+++ resources/inputs/basic-select.input.tsx
@@ -14,32 +14,68 @@ interface Props {
 
 export const BasicSelect = (props: Props) => {
   return (
-    <Select
-      name={props.name}
-      value={props.value}
-      disabled={props.disabled}
-      data-testid={props.testId}
-      onChange={(e) => props.onChange(e.target.value)}
-      style={{ width: props.width }}
-      className={props.className}
-    >
-      {props.options.map((option, index) => (
-        <option key={index} value={option.value}>
-          {option.value === props.value
-            ? option.selectedLabel || option.label
-            : option.label}
-        </option>
-      ))}
-    </Select>
+    <SelectWrapper width={props.width} className={props.className}>
+      <Select
+        name={props.name}
+        value={props.value}
+        disabled={props.disabled}
+        data-testid={props.testId}
+        onChange={(e) => props.onChange(e.target.value)}
+      >
+        {props.options.map((option, index) => (
+          <option key={index} value={option.value}>
+            {option.value === props.value
+              ? option.selectedLabel || option.label
+              : option.label}
+          </option>
+        ))}
+      </Select>
+    </SelectWrapper>
   );
 };
 
+const SelectWrapper = styled.div<{ width?: string }>`
+  position: relative;
+  width: ${props => props.width || '100%'};
+
+  &::after {
+    content: '▼';
+    font-size: 1vh;
+    top: 50%;
+    right: 10px;
+    transform: translateY(-50%);
+    position: absolute;
+    pointer-events: none;
+  }
+`;
+
 const Select = styled.select`
+  appearance: none;
+  width: 100%;
   padding: 8px;
+  padding-right: 30px; /* Space for the custom arrow */
   border: 1px solid var(--gray-200);
   border-radius: 4px;
   height: 6vh;
   color: black;
   font-size: 2vh;
   font-family: "Montserrat", sans-serif;
+  background-color: white;
+  cursor: pointer;
+
+  text-align: center;
+  text-align-last: center;
+
+  &::-ms-expand {
+    display: none;
+  }
+
+  &:focus {
+    outline: none;
+    border-color: var(--primary-color);
+  }
+
+  option {
+    text-align: center;
+  }
 `;
