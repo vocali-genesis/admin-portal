154518bf63b6e390f730cfd308676c14978ac558
Abel Bordonado <abelbordonado@gmail.com>
2024-09-11 12:12:33 +0800

    Changed favicon transparent

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,7 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&family=Montserrat:wght@300;400;500;600;700&display=swap"
           rel="stylesheet"
         />
-        <link rel="icon" href="/glogo.png" />
+        <link rel="icon" href="/gLogo-transparent.png" />
       </Head>
       <body>
         <Main />
