41696263e4a28d5ee748a9d213addc6dc47b26a8
kayiganwa04 <kayiganwa04@gmail.com>
2024-09-12 12:32:15 +0200

    added chnges specifically for pdf

--- modules/recording/recording/report.tsx
+++ modules/recording/recording/report.tsx
@@ -9,6 +9,7 @@ import Button from "@/resources/containers/button";
 import Download from "./libs/download";
 import { ProgressBar } from "@/resources/containers/progress-bar";
 import ViewContentEditable from "@/resources/containers/view-content-editable";
+import { useReactToPrint } from 'react-to-print';
 import { SubscriptionGuard } from "@/resources/guards/subscription.guard";
 
 const Report = () => {
@@ -22,7 +23,6 @@ const Report = () => {
   const [transcriptionContent, setTranscriptionContent] = useState<string[]>(
     [],
   );
-
   const [time, setTime] = useState({ transcription: 0, report: 0 });
   const [isDownloadOpen, setIsDownloadOpen] = useState(false);
   const audioRef = useRef<HTMLAudioElement>(null);
@@ -48,7 +48,21 @@ const Report = () => {
     setActiveTab(tab);
   };
 
-  // TODO: Refactor as a resource
+  const componentRef = useRef<HTMLDivElement>(null);
+  const [hideIcons, setHideIcons] = useState(false)
+  const downloadPdf = useReactToPrint({
+    content: () => componentRef.current,
+    documentTitle: 'Report',
+    onAfterPrint: () => setHideIcons(false),
+  });
+
+  const handleDownloadPdf = () => {
+    setHideIcons(true);
+    setTimeout(() => {
+      downloadPdf();
+    }, 300);
+  };
+
   const renderProgressBar = () => {
     const totalTime = time.transcription + time.report;
     const transcriptionWidth = (time.transcription / totalTime) * 100;
@@ -99,7 +113,7 @@ const Report = () => {
   const renderContent = () => {
     return (
       <div className={report_styles.viewContainer}>
-        <div
+        <div ref={componentRef}
           className={
             activeTab === "report"
               ? report_styles.visibleContent
@@ -110,6 +124,7 @@ const Report = () => {
             ([title, content], index: number) => {
               return (
                 <ViewContentEditable
+                  hideIcons={hideIcons}
                   key={index}
                   title={title}
                   content={content}
@@ -142,7 +157,7 @@ const Report = () => {
         }
         break;
       case "report":
-        await Download.downloadReport(reportContent);
+        handleDownloadPdf();  // Triggers the report download
         break;
       case "transcription":
         await Download.downloadTranscription(transcriptionContent);
@@ -172,7 +187,7 @@ const Report = () => {
             <button onClick={() => void handleDownload("audio")}>
               {t("recording.download-audio")}
             </button>
-            <button onClick={() => void handleDownload("report")}>
+            <button onClick={() => handleDownload("report")}>
               {t("recording.download-report")}
             </button>
             <button onClick={() => void handleDownload("transcription")}>
@@ -184,7 +199,6 @@ const Report = () => {
     );
   }
 
-  // TODO: We shall have a generic tab resource (including the visualization)
   function renderTabs() {
     return (
       <div className={report_styles.tabs}>
@@ -225,6 +239,7 @@ const Report = () => {
       </div>
     );
   }
+
   return (
     <div className={report_styles.reportContainer}>
       <div className={`${report_styles.topContainer}`}>
@@ -246,28 +261,7 @@ const Report = () => {
             ? t("recording.pause-audio")
             : t("recording.replay-audio")}
         </Button>
-        <div className="flex" style={{ gap: "8px" }}>
-          <Button
-            onClick={() => void router.push("/app/dashboard")}
-            variant="primary"
-            className={report_styles.newRecordingButton}
-          >
-            {t("recording.new-recording")}
-          </Button>
-        </div>
       </div>
-      <audio
-        ref={audioRef}
-        src={audioUrl as string}
-        onLoadedMetadata={() => {
-          console.log({ duration: audioRef.current?.duration });
-          setAudioDuration(audioRef.current?.duration || 0);
-        }}
-        style={{ display: "none" }}
-        onEnded={() => setIsAudioPlaying(false)}
-        onPause={() => setIsAudioPlaying(false)}
-        onPlay={() => setIsAudioPlaying(true)}
-      />
     </div>
   );
 };
