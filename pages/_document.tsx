9edc2d28515a67ea762de08a1ef8c26f96f67337
Abel Bordonado <abelbordonado@gmail.com>
2024-09-13 13:41:49 +0800

    removed h1

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <h1>Genesis</h1>
+        <title>Genesis</title>
         <link
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&family=Montserrat:wght@300;400;500;600;700&display=swap"
           rel="stylesheet"
